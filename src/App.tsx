bfea5dd08f40438ec2cbd3389ab11c3f85278273
Renato Magrini <renatomagrini@gmail.com>
2022-08-17 22:41:23 +0200

    Created React Project using VITE

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// Hooks can only be used inside function components
+// Hooks must always be executed in the same order at the top level of function components
+// Hooks can not be put inside if statements, loops or functions
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          fontFamily: "sans-serif",
+          textAlign: "center",
+          marginTop: "100px",
+        }}
+      >
+        <h1>React Hooks</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
